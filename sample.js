33f7b7b7a3bd4d274d40abca2c7844527fead568
meet2lax <37535512+meet2lax@users.noreply.github.com>
2019-06-06 14:11:16 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,158 @@
+'use strict';
+//var NS = 'org.acme.mynetwork';
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Testing samples
+ */
+
+'use strict';
+
+// Test program to test the business network against the embedded runtime that stores the state of the blockchain in-memory.  It allows one to ocus on testing
+// the business logic rather than configuring an entire Fabric.
+
+const AdminConnection = require('composer-admin').AdminConnection;
+const BrowserFS = require('browserfs/dist/node/index');
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+//const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+const path = require('path');
+
+require('chai').should();
+
+const bfs_fs = BrowserFS.BFSRequire('fs');
+const NS = 'org.acme.mynetwork';
+
+describe('Commodity Trading', () => {
+
+    // In-memory card store for testing so cards are not persisted to the file system
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+    let adminConnection;
+    let businessNetworkConnection;
+
+    before(async () => {
+        // Embedded connection used for local testing
+        const connectionProfile = {
+            name: 'embedded',
+            'x-type': 'embedded'
+        };
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
+
+        // PeerAdmin identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+
+        const deployerCardName = 'PeerAdmin';
+        adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
+    });
+
+    beforeEach(async () => {
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+
+        const adminUserName = 'admin';
+        let adminCardName;
+        let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+
+        // Install the Composer runtime for the new business network
+        await adminConnection.install(businessNetworkDefinition);
+
+        // Start the business network and configure an network admin identity
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: adminUserName,
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkDefinition.getName(), businessNetworkDefinition.getVersion(), startOptions);
+
+        // Import the network admin identity for us to use
+        adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`;
+        await adminConnection.importCard(adminCardName, adminCards.get(adminUserName));
+
+        // Connect to the business network using the network admin identity
+        await businessNetworkConnection.connect(adminCardName);
+    });
+
+
+    describe('#tradeCommodity', () => {
+
+        it('should be able to trade a commodity', () => {
+            const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+            // create the traders
+            const dan = factory.newResource(NS, 'Trader', 'dan@email.com');
+            dan.firstName = 'Dan';
+            dan.lastName = 'Selman';
+
+            const simon = factory.newResource(NS, 'Trader', 'simon@email.com');
+            simon.firstName = 'Simon';
+            simon.lastName = 'Stone';
+
+            // create the commodity
+            const commodity = factory.newResource(NS, 'Commodity', 'EMA');
+            commodity.description = 'Corn';
+            commodity.mainExchange = 'Euronext';
+            commodity.quantity = 100;
+            commodity.owner = factory.newRelationship(NS, 'Trader', dan.$identifier);
+
+            // create the trade transaction
+            const trade = factory.newTransaction(NS, 'Trade');
+            trade.newOwner = factory.newRelationship(NS, 'Trader', simon.$identifier);
+            trade.commodity = factory.newRelationship(NS, 'Commodity', commodity.$identifier);
+
+            // the owner should of the commodity should be dan
+            commodity.owner.$identifier.should.equal(dan.$identifier);
+
+            // Get the asset registry.
+            return businessNetworkConnection.getAssetRegistry(NS + '.Commodity')
+                .then((assetRegistry) => {
+
+                    // add the commodity to the asset registry.
+                    return assetRegistry.add(commodity)
+                        .then(() => {
+                            return businessNetworkConnection.getParticipantRegistry(NS + '.Trader');
+                        })
+                        .then((participantRegistry) => {
+                            // add the traders
+                            return participantRegistry.addAll([dan, simon]);
+                        })
+                        .then(() => {
+                            // submit the transaction
+                            return businessNetworkConnection.submitTransaction(trade);
+                        })
+                        .then(() => {
+                            return businessNetworkConnection.getAssetRegistry(NS + '.Commodity');
+                        })
+                        .then((assetRegistry) => {
+                            // re-get the commodity
+                            return assetRegistry.get(commodity.$identifier);
+                        })
+                        .then((newCommodity) => {
+                            // the owner of the commodity should not be simon
+                            newCommodity.owner.$identifier.should.equal(simon.$identifier);
+                        });
+                });
+        });
+    });
+});
